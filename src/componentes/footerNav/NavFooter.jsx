8255210197893bf2314ce63870288ae45652df73
Gabriel Alejandro Leal <68304965+ParkerPiter@users.noreply.github.com>
2023-12-04 17:52:48 -0400

    Update NavFooter.jsx

--- src/componentes/footerNav/NavFooter.jsx
+++ src/componentes/footerNav/NavFooter.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
+import '../../../windows-95-ui-kit/css/w95.css'
 import style from "./NavFooter.module.css";
 import on from "../../assets/musicon.png";
 import off from "../../assets/musicoff.png";
 import useSound from "use-sound";
-import musica from "../../assets/musica.mp3";
+import musica from "../../assets/Song_ath_WEB.MP3"
 
 const Nav = ({
   handlePlay,
@@ -99,178 +100,178 @@ const Nav = ({
   hours = hours % 12 || 12;
 
   return (
-    <div className={style.footer}>
-      <div className={style.sinMargin}>
-        <p
-          className={`${style.start} btn`}
-          style={{
-            paddingRight: "30px",
-            marginTop: "-0.5px",
-            marginBottom: "-2px",
-          }}
-          onClick={handleStartClick}
-        >
-          <img
-            className={style.win}
-            src="https://img.icons8.com/color/48/windows-95.png"
-            alt="windows-95"
-            onClick={handleStartClick}
-          />
-          Start
-        </p>
-        {isAccordionOpen && (
-          <div
-            className={style.optionList}
+      <div className={style.footer}>
+        <div className={style.sinMargin}>
+          <p
+            className={`${style.start} btn`}
             style={{
-              position: "fixed",
-              bottom: "45px",
-              transform: "scaleY(-1)",
+              paddingRight: "5px",
+              marginTop: "-0.5px",
+              marginBottom: "-2px",
             }}
+            onClick={handleStartClick}
           >
-            <ul
-              className={style.borderAccor}
+            <img
+              className={style.win}
+              src="https://img.icons8.com/color/48/windows-95.png"
+              alt="windows-95"
+              onClick={handleStartClick}
+            />
+            Start
+          </p>
+          {isAccordionOpen && (
+            <div
+              className={style.optionList}
               style={{
-                backgroundColor: "gray",
-                padding: "5px 25px 5px 25px",
-                marginBottom: "0px",
-                marginLeft: "-15px",
+                position: "fixed",
+                bottom: "45px",
+                transform: "scaleY(-1)",
               }}
             >
-              <li
-                onClick={handleOptionClick}
-                id="twitter"
-                className={style.hovers}
-                style={{ transform: "scaleY(-1)", padding: "5px" }}
+              <ul
+                className={style.borderAccor}
+                style={{
+                  backgroundColor: "gray",
+                  padding: "5px 25px 5px 25px",
+                  marginBottom: "0px",
+                  marginLeft: "-15px",
+                }}
               >
-                <img
-                  style={{ width: "25px", height: "25px", marginRight: "7px" }}
-                  src={twitter}
-                  alt="twitter"
-                />
-                Twitter
-              </li>
-              <li
-                onClick={handleOptionClick}
-                id="token"
-                className={style.hovers}
-                style={{ transform: "scaleY(-1)", padding: "5px" }}
-              >
-                <img
-                  style={{ width: "25px", height: "25px", marginRight: "7px" }}
-                  src={trash}
-                  alt="trash"
-                />
-                Token Burn
-              </li>
-              <li
-                onClick={handleOptionClick}
-                id="telegram"
-                className={style.hovers}
-                style={{ transform: "scaleY(-1)", padding: "5px" }}
-              >
-                <img
-                  style={{ width: "25px", height: "25px", marginRight: "7px" }}
-                  src={telegram}
-                  alt="telegram"
-                />
-                Telegram
-              </li>
-              <li
-                onClick={handleOptionClick}
-                id="contract"
-                className={style.hovers}
-                style={{ transform: "scaleY(-1)", padding: "5px" }}
-              >
-                <img
-                  style={{ width: "25px", height: "25px", marginRight: "7px" }}
-                  src={contract}
-                  alt="contract"
-                />
-                Contract
-              </li>
-              <li
-                onClick={handleOptionClick}
-                id="memes"
-                className={style.hovers}
-                style={{ transform: "scaleY(-1)", padding: "5px" }}
-              >
-                <img
-                  style={{ width: "25px", height: "25px", marginRight: "7px" }}
-                  src={meme}
-                  alt="meme"
-                />
-                Memes
-              </li>
-              <li
-                onClick={handleOptionClick}
-                id="game"
-                className={style.hovers}
-                style={{ transform: "scaleY(-1)", padding: "5px" }}
-              >
-                <img
-                  style={{ width: "25px", height: "25px", marginRight: "7px" }}
-                  src={game}
-                  alt="game"
-                />
-                Games
-              </li>
-              <li
-                onClick={handleOptionClick}
-                id="chat"
-                className={style.hovers}
-                style={{ transform: "scaleY(-1)", padding: "5px" }}
-              >
-                <img
-                  style={{ width: "25px", height: "25px", marginRight: "7px" }}
-                  src={mdos}
-                  alt="mdos"
-                />
-                Chat Bot
-              </li>
-              <li
-                id="opcion1"
-                style={{ transform: "scaleY(-1)", padding: "5px" }}
-              >
-                <img
-                  style={{ width: "25px", height: "25px", marginRight: "7px" }}
-                  src={pc}
-                  alt="pc"
-                />
-                Chart
-              </li>
-              <div className={style.barraGris}></div>
-              <p className={style.ath}>HTA</p>
-            </ul>
-          </div>
-        )}
-      </div>
-      <div className={` ${style.clock}`}>
-        <div className={style.soundIcons}>
-          <img
-            className={`${style.audioOn} ${style.on1}`}
-            src={on}
-            alt=""
-            onClick={handlePlay}
-          />
-          <img
-            className={`${style.audioOn} ${style.off}`}
-            src={off}
-            alt=""
-            onClick={handleStop}
-          />
-          <img
-            className={`${style.audioOn} ${style.on2}`}
-            src={on}
-            alt=""
-            onClick={handlePlay}
-          />
+                <li
+                  onClick={handleOptionClick}
+                  id="twitter"
+                  className={style.hovers}
+                  style={{ transform: "scaleY(-1)", padding: "5px" }}
+                >
+                  <img
+                    style={{ width: "25px", height: "25px", marginRight: "7px" }}
+                    src={twitter}
+                    alt="twitter"
+                  />
+                  Twitter
+                </li>
+                <li
+                  onClick={handleOptionClick}
+                  id="token"
+                  className={style.hovers}
+                  style={{ transform: "scaleY(-1)", padding: "5px" }}
+                >
+                  <img
+                    style={{ width: "25px", height: "25px", marginRight: "7px" }}
+                    src={trash}
+                    alt="trash"
+                  />
+                  Token Burn
+                </li>
+                <li
+                  onClick={handleOptionClick}
+                  id="telegram"
+                  className={style.hovers}
+                  style={{ transform: "scaleY(-1)", padding: "5px" }}
+                >
+                  <img
+                    style={{ width: "25px", height: "25px", marginRight: "7px" }}
+                    src={telegram}
+                    alt="telegram"
+                  />
+                  Telegram
+                </li>
+                <li
+                  onClick={handleOptionClick}
+                  id="contract"
+                  className={style.hovers}
+                  style={{ transform: "scaleY(-1)", padding: "5px" }}
+                >
+                  <img
+                    style={{ width: "25px", height: "25px", marginRight: "7px" }}
+                    src={contract}
+                    alt="contract"
+                  />
+                  Contract
+                </li>
+                <li
+                  onClick={handleOptionClick}
+                  id="memes"
+                  className={style.hovers}
+                  style={{ transform: "scaleY(-1)", padding: "5px" }}
+                >
+                  <img
+                    style={{ width: "25px", height: "25px", marginRight: "7px" }}
+                    src={meme}
+                    alt="meme"
+                  />
+                  Memes
+                </li>
+                <li
+                  onClick={handleOptionClick}
+                  id="game"
+                  className={style.hovers}
+                  style={{ transform: "scaleY(-1)", padding: "5px" }}
+                >
+                  <img
+                    style={{ width: "25px", height: "25px", marginRight: "7px" }}
+                    src={game}
+                    alt="game"
+                  />
+                  Games
+                </li>
+                <li
+                  onClick={handleOptionClick}
+                  id="chat"
+                  className={style.hovers}
+                  style={{ transform: "scaleY(-1)", padding: "5px" }}
+                >
+                  <img
+                    style={{ width: "25px", height: "25px", marginRight: "7px" }}
+                    src={mdos}
+                    alt="mdos"
+                  />
+                  Chat Bot
+                </li>
+                <li
+                  id="opcion1"
+                  style={{ transform: "scaleY(-1)", padding: "5px" }}
+                >
+                  <img
+                    style={{ width: "25px", height: "25px", marginRight: "7px" }}
+                    src={pc}
+                    alt="pc"
+                  />
+                  Chart
+                </li>
+                <div className={style.barraGris}></div>
+                <p className={style.ath}>HTA</p>
+              </ul>
+            </div>
+          )}
         </div>
-        <p className={style.clock}>
-          {`${hours}:${min} ${pmAm}`}
-        </p>
+        <div className={` ${style.clock}`}>
+          <div className={style.soundIcons}>
+            <img
+              className={`${style.audioOn} ${style.on1}`}
+              src={on}
+              alt=""
+              onClick={handlePlay}
+            />
+            <img
+              className={`${style.audioOn} ${style.off}`}
+              src={off}
+              alt=""
+              onClick={handleStop}
+            />
+            <img
+              className={`${style.audioOn} ${style.on2}`}
+              src={on}
+              alt=""
+              onClick={handlePlay}
+            />
+          </div>
+          <p className={style.timer}>
+            {`${hours}:${min} ${pmAm}`}
+          </p>
+        </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default Nav;
